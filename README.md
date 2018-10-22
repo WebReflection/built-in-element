5d0f6d2e69c19f6d96a0d9999d5381d7cedf508b
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-10-22 22:03:59 +0200

    starting from the scratch

---
+++ README.md
@@ -0,0 +1,45 @@
+# built-in-element
+
+[![Build Status](https://travis-ci.com/WebReflection/built-in-element.svg?branch=master)](https://travis-ci.com/WebReflection/built-in-element) ![WebReflection status](https://offline.report/status/webreflection.svg)
+
+A polyfill for Custom Elements built-in that patches the native registry in Safari and WebKit and it's [compatible with the Custom Elements polyfill](https://github.com/WebReflection/document-register-element).
+
+It doesn't touch Chrome and Firefox 63+ with native support, and it weights less than 1K.
+
+### Caveat
+
+You cannot use the `constructor` in any meaningful way if you want to ensure API consistency.
+
+Create new elements via `document.createElement('button', {is: 'my-button'})` but do not use `new MyButton` or incompatible browsers will throw right away because they made `HTMLButtonElement` and all others not usable as classes.
+
+If you need a reliable entry point to setup your custom builtins use the `connectedCallback` method instead of the `constructor` so you're also sure all attributes are eventually already known and you'll have full control.
+
+Alternatively, use a `WeakSet` to optionally invoke a setup.
+
+```js
+const initialized = new WeakSet;
+const setup = node => {
+  initialized.add(node);
+  node.live = true;
+};
+class MyButton extends HTMLButtonElement {
+  connectedCallback() {
+    if (!initialized.has(this))
+      setup(this);
+    // anything else
+  }
+}
+```
+
+You can do the same at the beginning of `attributeChangedCallback`.
+
+### Compatible with ...
+
+Any engine that supports genuine ES2015 syntax and the following features:
+
+  * global `MutationObserver`, `customElements`, and `Promise`
+  * `assign`, `create`, `defineProperties`, and `setPrototypeOf` from the `Object`
+
+Alternatively, you can polyfill custom elements upfront and use Babel 7 to target older browsers.
+
+Feel free to live test the [native ES2015 version](https://webreflection.github.io/built-in-element/test/) or the [transpiled one](https://webreflection.github.io/built-in-element/test/es5/).
---
+++ index.js
@@ -0,0 +1,150 @@
+/*! (c) Andrea Giammarchi - ISC */
+(function (document, customElements) {'use strict';
+  if (customElements.get('li-li'))
+    return;
+  const EXTENDS = 'extends';
+  try {
+    class LI extends HTMLLIElement {}
+    customElements.define('li-li', LI, {[EXTENDS]: 'li'});
+    if (!/is="li-li"/.test((new LI).outerHTML))
+      throw {};
+  } catch (o_O) {
+    const ATTRIBUTE_CHANGED_CALLBACK = 'attributeChangedCallback';
+    const CONNECTED_CALLBACK = 'connectedCallback';
+    const DISCONNECTED_CALLBACK = 'disconnectedCallback';
+    const {assign, create, defineProperties, setPrototypeOf} = Object;
+    const {define, get, upgrade, whenDefined} = customElements;
+    const registry = create(null);
+    const attributeChanged = changes => {
+      for (let i = 0, length = changes.length; i < length; i++) {
+        const {attributeName, oldValue, target} = changes[i];
+        const newValue = target.getAttribute(attributeName);
+        if (
+          ATTRIBUTE_CHANGED_CALLBACK in target &&
+          !(oldValue == newValue && newValue == null)
+        )
+          target[ATTRIBUTE_CHANGED_CALLBACK](
+            attributeName,
+            oldValue,
+            target.getAttribute(attributeName),
+            // TODO: add getAttributeNS if the node is XML
+            null
+          );
+      }
+    };
+    const getInfo = node => {
+      let is = node.getAttribute('is');
+      if (is) {
+        is = is.toLowerCase();
+        if (is in registry)
+          return registry[is];
+      }
+      return null;
+    };
+    const setup = (node, info) => {
+      const {Class} = info;
+      const oa = Class.observedAttributes || [];
+      setPrototypeOf(node, Class.prototype);
+      if (oa.length) {
+        new MutationObserver(attributeChanged).observe(
+          node,
+          {attributeFilter: oa, attributeOldValue: true}
+        );
+        const changes = [];
+        for (let i = 0, length = oa.length; i < length; i++)
+          changes.push({attributeName: oa[i], oldValue: null, target: node});
+        attributeChanged(changes);
+      }
+    };
+    const setupIfNeeded = node => {
+      if (node.nodeType !== 1)
+        return;
+      const info = getInfo(node);
+      if (info) {
+        if (!(node instanceof info.Class))
+          setup(node, info);
+        if (CONNECTED_CALLBACK in node)
+          node[CONNECTED_CALLBACK]();
+      }
+    };
+    new MutationObserver(changes => {
+      for (let i = 0, length = changes.length; i < length; i++) {
+        const {addedNodes, removedNodes} = changes[i];
+        for (let j = 0, len = addedNodes.length; j < len; j++)
+          setupIfNeeded(addedNodes[j]);
+        for (let j = 0, len = removedNodes.length; j < len; j++) {
+          const node = removedNodes[j];
+          const info = getInfo(node);
+          if (
+            info &&
+            node instanceof info.Class &&
+            DISCONNECTED_CALLBACK in node
+          )
+            node[DISCONNECTED_CALLBACK]();
+        }
+      }
+    }).observe(
+      document,
+      {childList: true, subtree: true}
+    );
+    defineProperties(
+      customElements,
+      {
+        define: {
+          value(name, Class, options) {
+            name = name.toLowerCase();
+            if (options && EXTENDS in options) {
+              // currently options is not used but preserved for the future
+              registry[name] = assign({}, options, {Class});
+              const query = options[EXTENDS] + '[is="' + name + '"]';
+              const changes = document.querySelectorAll(query);
+              for (let i = 0, length = changes.length; i < length; i++)
+                setupIfNeeded(changes[i]);
+            }
+            else
+              define.apply(customElements, arguments);
+          }
+        },
+        get: {
+          value(name) {
+            return name in registry ?
+              registry[name].Class :
+              get.call(customElements, name);
+          }
+        },
+        upgrade: {
+          value(node) {
+            const info = getInfo(node);
+            if (info && !(node instanceof info.Class))
+              setup(node, info);
+            else
+              upgrade.call(customElements, node);
+          }
+        },
+        whenDefined: {
+          value(name) {
+            return name in registry ?
+              Promise.resolve() :
+              whenDefined.call(customElements, name);
+          }
+        }
+      }
+    );
+    const {createElement} = document;
+    defineProperties(
+      document,
+      {
+        createElement: {
+          value(name, options) {
+            const node = createElement.call(document, name);
+            if (options && 'is' in options) {
+              node.setAttribute('is', options.is);
+              customElements.upgrade(node);
+            }
+            return node;
+          }
+        }
+      }
+    );
+  }
+}(document, customElements));
